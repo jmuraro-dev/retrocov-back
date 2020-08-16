4a25e0628087ba54db56e669e07507c016e71b9c
valerian-haury <haury.valerian@gmail.com>
2020-08-16 20:35:35 +0200

    added data verification

--- api/restaurant/update_password.php
+++ api/restaurant/update_password.php
@@ -20,29 +20,44 @@ $restaurant = new Restaurant($db);
 // get id of restaurant to be edited
 $data = json_decode(file_get_contents("php://input"));
 
+// make sure data is not empty
+if (
+    !empty($data->id) &&
+    !empty($data->token) &&
+    !empty($data->password)
+) {
+
 // set ID property of restaurant to be edited
-$restaurant->id = $data->id;
-$restaurant->token = $data->token;
-$restaurant->password = $data->token;
+    $restaurant->id = $data->id;
+    $restaurant->token = $data->token;
+    $restaurant->password = $data->password;
 
 
 // update the restaurant
-if ($restaurant->updatePassword()) {
+    if ($restaurant->updatePassword()) {
 
-    // set response code - 200 ok
-    http_response_code(200);
+        // set response code - 200 ok
+        http_response_code(200);
+
+        // tell the user
+        echo json_encode(
+            array("message" => "Restaurant password was updated.")
+        );
+    } // if unable to update the restaurant, tell the user
+    else {
+
+        // set response code - 503 service unavailable
+        http_response_code(503);
+
+        // tell the user
+        echo json_encode(array("message" => "Unable to update restaurant password."));
+    }
+} else {
+
+    // set response code - 400 bad request
+    http_response_code(400);
 
     // tell the user
-    echo json_encode(
-        array("message" => "Restaurant password was updated.")
-    );
-} // if unable to update the restaurant, tell the user
-else {
-
-    // set response code - 503 service unavailable
-    http_response_code(503);
-
-    // tell the user
-    echo json_encode(array("message" => "Unable to update restaurant password."));
+    echo json_encode(array("message" => "Unable to update Restaurant. Data is incomplete."));
 }
 ?>
